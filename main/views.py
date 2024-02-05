e17c6e3f21e02311e5918e7aa970366d5e0c4172
xzxbtl <xzxbtl@mail.ru>
2024-02-05 03:07:41 +0300

    Rebase html and urls Also add base.html

--- main/views.py
+++ main/views.py
@@ -3,8 +3,18 @@ from django.shortcuts import HttpResponse
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    content = {
+        'title': 'Home - Main',
+        'content': 'Furniture Shop Home'
+    }
+    return render(request, 'main/index.html', content)
 
 
 def about(request):
-    return render(request, 'main/about.html')
+    content = {
+        'title': 'Home - About us',
+        'content': 'About us',
+        'text_on_page': 'We have been operating since 2004, we have more than 100 thousand customers and about 34 '
+                        'stores across the country! you can view the reviews of our products if in doubt'
+    }
+    return render(request, 'main/about.html', content)
--- shopsite/urls.py
+++ shopsite/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('', include('main.urls', namespace='main')) # namespace к какому сайту относятся
 ]
