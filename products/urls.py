ba71092f6ec48b8f9f07758d3d2affd503305ff6
xzxbtl <xzxbtl@mail.ru>
2024-02-11 23:51:00 +0300

    slug convector for categories

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from . import views
 app_name = 'products'
 
 urlpatterns = [
-    path('', views.catalog, name="index"),
+    path('<slug:category_slug>/', views.catalog, name="index"),
     path('product/<slug:product_slug>/', views.product, name="product")
 ]
--- products/views.py
+++ products/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from products.models import Products
 
 
-def catalog(request):
-    products = Products.objects.all()
+def catalog(request, category_slug):
+    if category_slug == 'all':
+        products = Products.objects.all()
+    else:
+        products = get_list_or_404(Products.objects.filter(category__slug=category_slug))  # Если нету в категории
+        # нечего ошибка 404
 
     context = {
         'title': 'Home - Catalog',
