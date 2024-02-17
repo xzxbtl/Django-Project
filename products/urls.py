c57ba48386eea4e1acb8972f524afd7e3f3de6eb
xzxbtl <xzxbtl@mail.ru>
2024-02-17 19:49:04 +0300

    before postgresql + new fixtures also added search and Q objects

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,7 @@ from . import views
 app_name = 'products'
 
 urlpatterns = [
+    path('search/', views.catalog, name="search"),
     path('<slug:category_slug>/', views.catalog, name="index"),
     path('product/<slug:product_slug>/', views.product, name="product")
 ]
--- products/views.py
+++ products/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render, get_list_or_404
 from products.models import Products
 from django.core.paginator import Paginator
+from .utils import q_search
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug=None):
     page = request.GET.get('page', 1)
     on_sale = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', None)
+    query = request.GET.get('q', None)
 
     if category_slug == 'all':
         products = Products.objects.all()
+    elif query:
+        products = q_search(query)
     else:
         products = get_list_or_404(Products.objects.filter(category__slug=category_slug))  # Если нету в категории
         # нечего ошибка 404
