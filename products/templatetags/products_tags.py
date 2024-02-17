a61684f06e56df9dd65e93f3d289e2a6e6932f2b
xzxbtl <xzxbtl@mail.ru>
2024-02-17 18:12:17 +0300

    filters products + products_tags and urlencode

--- products/templatetags/products_tags.py
+++ products/templatetags/products_tags.py
@@ -1,3 +1,5 @@
+from django.utils.http import urlencode
+
 from products.models import Category
 from django import template
 
@@ -7,3 +9,11 @@ register = template.Library()
 @register.simple_tag()
 def tag_categories():
     return Category.objects.all()
+
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
+
--- products/views.py
+++ products/views.py
@@ -5,12 +5,20 @@ from django.core.paginator import Paginator
 
 def catalog(request, category_slug):
     page = request.GET.get('page', 1)
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
 
     if category_slug == 'all':
         products = Products.objects.all()
     else:
         products = get_list_or_404(Products.objects.filter(category__slug=category_slug))  # Если нету в категории
         # нечего ошибка 404
+
+    if on_sale:
+        products = products.filter(discount__gt=0)
+
+    if order_by and order_by != "default":
+        products = products.order_by(order_by)
 
     paginator = Paginator(products, 3)
     current_page = paginator.page(int(page))
